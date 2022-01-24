c68fe0a496211954a9c8d936c95dd35750a23ea6
한신웅 <hsuj86@gmail.com>
2022-01-24 17:50:37 +0900

    delete dist folder & strictmode

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import './styles/reset.scss';
 import './styles/common.scss';
 import Routes from './Routes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes />, document.getElementById('root'));
