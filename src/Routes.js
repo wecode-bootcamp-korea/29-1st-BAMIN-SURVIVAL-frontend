9e1b9775eacac6312b65b71a0062682a35d1892a
한신웅 <hsuj86@gmail.com>
2022-01-25 20:08:43 +0900

    make footer

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
+import Footer from './pages/Shinung/Footer/Footer';
 import Nav from './pages/Shinung/Nav/Nav';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
+      <Footer />
       <Routes />
     </BrowserRouter>
   );
