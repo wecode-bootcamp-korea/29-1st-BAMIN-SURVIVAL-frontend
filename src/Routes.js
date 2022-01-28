9c23d875ee47fb360b71bdfa592e8632398945e8
한신웅 <hsuj86@gmail.com>
2022-01-28 11:07:04 +0900

    routes.js : delete login components

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './pages/Shinung/Nav/Nav';
-import LoginBaeminSurvival from './pages/Shinung/Pages/LogIn/Login';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
-      <Routes>
-        <Route path="/login" element={<LoginBaeminSurvival />} />
-      </Routes>
+      <Routes />
     </BrowserRouter>
   );
 }
