c94562ca95c13dadd8c729729ea833ebf5f22123
한신웅 <hsuj86@gmail.com>
2022-01-28 11:02:52 +0900

    modify routes, variables, ignore

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import MainTest from './pages/Shinung/MainTest';
 import Nav from './pages/Shinung/Nav/Nav';
 import LoginBaeminSurvival from './pages/Shinung/Pages/LogIn/Login';
 
@@ -8,7 +7,6 @@ function Router() {
   return (
     <BrowserRouter>
       <Nav />
-      {/* <MainTest /> */}
       <Routes>
         <Route path="/login" element={<LoginBaeminSurvival />} />
       </Routes>
