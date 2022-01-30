e06dcaa975d171010aeb3a521a6e6b8e70078b52
한신웅 <hsuj86@gmail.com>
2022-01-30 18:15:02 +0900

    Nav폴더 위치 변경 및 Shinung 폴더 삭제

--- src/pages/Nav/Nav.js
+++ src/pages/Nav/Nav.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import CATEGORIES from './CategoryData';
 import Category from './Category';
-// import RecentSearch from './RecentSearch/RecentSearch';
 import './Nav.scss';
 import UserInfo from './UserInfo';
 import Search from './Search';
 
 const Nav = () => {
   const [searchInput, setSearchInput] = useState(true);
-  // const [modal, setModal] = useState(false);
 
   const listenScrollEvent = () => {
     window.scrollY > 10 ? setSearchInput(false) : setSearchInput(true);
@@ -36,7 +34,6 @@ const Nav = () => {
           <img alt="logo" src="/images/Shinung/logo.png" className="navLogo" />
         </Link>
         {searchInput && <Search />}
-        {/* {modal && <RecentSearch value={inputValue} />} */}
       </div>
 
       {searchInput || (
