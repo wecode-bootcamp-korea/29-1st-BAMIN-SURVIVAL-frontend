773f049027c67165d5f341183d0d15f82142fe5e
ChaeHyunIM <klqwrx7004@gmail.com>
2022-02-04 16:51:45 +0900

    SignUp Login 컴포넌트 동일한 class명으로 인한 css 충돌 해결

--- src/Router.js
+++ src/Router.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import Nav from './pages/Nav/Nav';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
