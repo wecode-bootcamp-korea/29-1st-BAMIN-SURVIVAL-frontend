c51a7545aa18fbd8d74044b039391cf95b9e7873
한신웅 <hsuj86@gmail.com>
2022-02-07 16:25:39 +0900

    테스트 코드 삭제

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
-import Test from './components/Text/TestBox';
 
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
@@ -12,7 +11,6 @@ const Router = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Test />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
